5f2dcd41b85f69fa98b4f9edd1d8aeedb750025d
Suji Hwang <soojiya2003@gmail.com>
2025-04-30 22:36:51 +0900

    Design: 폰트 크기 수정

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -3,8 +3,8 @@ export default function Logo() {
   return (
     <>
       <div className="inline-block text-center">
-        <h1 className="cafe24 text-[24px] leading-none">de:caffeine</h1>
-        <span className="nanum-gothic-regular text-[12px] text-[#977A65] leading-none float-left pl-[18px]">
+        <h1 className="cafe24 text-[28px] leading-none">de:caffeine</h1>
+        <span className="nanum-gothic-regular text-[16px] text-[#977A65] leading-none float-left pl-[12px]">
           개발자들의 카페인
         </span>
       </div>
