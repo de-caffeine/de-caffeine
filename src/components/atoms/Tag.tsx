51fa23519eaa03f4d88653f1b9179fd431d1e6b6
Suji Hwang <soojiya2003@gmail.com>
2025-04-30 23:36:22 +0900

    Feat: tag 폰트 사이즈 및 수정  #27

--- src/components/atoms/Tag.tsx
+++ src/components/atoms/Tag.tsx
@@ -1,7 +1,7 @@
 export default function Tag() {
   return (
     <>
-      <span className="inline-flex items-center text-[12px] px-[9px] pt-[5px] pb-[6px] bg-[#D7CAB9] rounded-[2px]">
+      <span className="nanum-gothic-regular inline-flex items-center text-[12px] px-[9px] pt-[5px] pb-[6px] bg-[#D7CAB9] rounded-[2px]">
         text
       </span>
     </>
