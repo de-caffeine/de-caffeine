ec8de3a89f65b6f5281428327e661dc887570c13
kyoeun <lke159357@hufs.ac.kr>
2025-04-30 16:13:52 +0900

    feat: Icon 컴포넌트 생성

--- src/components/atoms/Icon.tsx
+++ src/components/atoms/Icon.tsx
@@ -1,7 +1,13 @@
-export default function Icon() {
-    return (
-        <>
-            <image></image>
-        </>
-    );
+export default function Icon({ name }: { name: "alarm" | "chat" }) {
+  return (
+    <button>
+      <img
+        src={`/src/assets/images/Icon${
+          name === "alarm" ? "Alram" : "Chat"
+        }.png`}
+        alt={name}
+        className={name === "alarm" ? "w-[34px] h-[34px]" : "w-[24px] h-[24px]"}
+      />
+    </button>
+  );
 }
