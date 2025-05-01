30ea577e46f611a5f5449be53dd8c774300073fc
Suji Hwang <soojiya2003@gmail.com>
2025-05-01 19:19:17 +0900

    Feat: cursor-pointer 추가

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -2,7 +2,7 @@ import "../../css/font.css";
 export default function Logo() {
   return (
     <>
-      <div className="inline-block text-center">
+      <div className="inline-block text-center cursor-pointer">
         <h1 className="cafe24 text-[28px] leading-none">de:caffeine</h1>
         <span className="nanum-gothic-regular text-[16px] text-[#977A65] leading-none float-left pl-[12px]">
           개발자들의 카페인
