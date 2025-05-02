e8ff6e2fca26295b2cd63fd423899fc64d024add
kyoeun <lke159357@hufs.ac.kr>
2025-05-02 18:40:20 +0900

    feat: Icon 컴포넌트 생성 수정

--- src/components/atoms/Icon.tsx
+++ src/components/atoms/Icon.tsx
@@ -7,59 +7,43 @@ import homepageIcon from "../../assets/images/homepageIcon.png";
 import completeIcon from "../../assets/images/completeIcon.png";
 import completeNotIcon from "../../assets/images/completeNotIcon.png";
 
-export default function Icon() {
+export default function Icon({
+  name,
+}: {
+  name:
+    | "alarmIcon"
+    | "chatIcon"
+    | "mailIcon"
+    | "githubIcon"
+    | "velogIcon"
+    | "homepageIcon"
+    | "completeIcon"
+    | "completeNotIcon";
+}) {
+  const src =
+    name === "alarmIcon"
+      ? alarmIcon
+      : name === "chatIcon"
+      ? chatIcon
+      : name === "mailIcon"
+      ? mailIcon
+      : name === "githubIcon"
+      ? githubIcon
+      : name === "velogIcon"
+      ? velogIcon
+      : name === "homepageIcon"
+      ? homepageIcon
+      : name === "completeIcon"
+      ? completeIcon
+      : completeNotIcon;
+
+  const className =
+    name === "alarmIcon" ? "w-[34px] h-[34px]" : "w-[24px] h-[24px]";
+
   return (
     <>
       <button type="button" className="cursor-pointer">
-        <img src={alarmIcon} alt="알림 아이콘" className="w-[34px] h-[34px]" />
-      </button>
-
-      <button type="button" className="cursor-pointer">
-        <img src={chatIcon} alt="채팅 아이콘" className="w-[24px] h-[24px]" />
-      </button>
-
-      <button type="button" className="cursor-pointer">
-        <img src={mailIcon} alt="메일 아이콘" className="w-[24px] h-[24px]" />
-      </button>
-
-      <button type="button" className="cursor-pointer">
-        <img
-          src={githubIcon}
-          alt="깃허브 아이콘"
-          className="w-[24px] h-[24px]"
-        />
-      </button>
-
-      <button type="button" className="cursor-pointer">
-        <img
-          src={velogIcon}
-          alt="벨로그주소 아이콘"
-          className="w-[24px] h-[24px]"
-        />
-      </button>
-
-      <button type="button" className="cursor-pointer">
-        <img
-          src={homepageIcon}
-          alt="홈페이지주소 아이콘"
-          className="w-[24px] h-[24px]"
-        />
-      </button>
-
-      <button type="button" className="cursor-pointer">
-        <img
-          src={completeIcon}
-          alt="완료 아이콘"
-          className="w-[24px] h-[24px]"
-        />
-      </button>
-
-      <button type="button" className="cursor-pointer">
-        <img
-          src={completeNotIcon}
-          alt="미완료 아이콘"
-          className="w-[24px] h-[24px]"
-        />
+        <img src={src} alt={name} className={className} />
       </button>
     </>
   );
