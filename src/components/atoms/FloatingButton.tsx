9c5b068bef9d5d02fa9ec741512a0dea34e67dc4
Suji Hwang <soojiya2003@gmail.com>
2025-05-02 20:35:12 +0900

    Feat: 재사용가능한 component로 다시 작성

--- src/components/atoms/FloatingButton.tsx
+++ src/components/atoms/FloatingButton.tsx
@@ -1,48 +1,20 @@
 import { twMerge } from "tailwind-merge";
-import chat from "../../assets/images/chat.png";
-import like from "../../assets/images/like.png";
-import plus from "../../assets/images/plus.png";
-import likeFilled from "../../assets/images/like_filled.png";
-import { useState } from "react";
+// import like from "../../assets/images/like.png";
+// import likeFilled from "../../assets/images/like_filled.png";
+
+// children 으로 icon 이미지 받아서 재사용
 
 const floatBase = `w-[60px] h-[60px] bg-[#A9907E] rounded-full shadow-[0_4px_4px_rgba(0,0,0,0.25)] flex items-center justify-center cursor-pointer`;
 
-// 추후 좋아요 api 사용해서 like 클릭 수 업데이트 -> useState 현재값
 export default function FloatingButton({
-  buttonType,
+  children,
 }: {
-  buttonType: "write" | "like" | "chat";
+  children: React.ReactNode;
 }) {
-  const [isLiked, setIsLiked] = useState(false);
-  const [count, setCount] = useState<number>(0);
-
-  const likeClickHandler = () => {
-    setIsLiked((prev) => !prev);
-    setCount((count) => count + (isLiked ? -1 : 1));
-  };
-
   return (
     <>
       <button type="button" className={twMerge(floatBase)}>
-        <img src={plus} alt="채팅" />
-      </button>
-      <button type="button" className={twMerge(floatBase)}>
-        <img src={chat} alt="채팅" className="relative top-0.5 right-[1px]" />
-        {/* 토글 좋아요 버튼 */}
-      </button>
-      <button
-        type="button"
-        onClick={likeClickHandler}
-        className={twMerge(
-          floatBase,
-          "flex-col leading-none text-[#ffffff] text-[12px]"
-        )}>
-        <img
-          className="mt-[6px] mb-0"
-          src={isLiked ? likeFilled : like}
-          alt={isLiked ? likeFilled : like}
-        />
-        {count}
+        {children}
       </button>
     </>
   );
