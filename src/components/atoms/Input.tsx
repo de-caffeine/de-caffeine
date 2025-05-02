d5d0b2b0f1acbdc6ed334b35b8a6d46a34ee2b96
kyoeun <lke159357@hufs.ac.kr>
2025-05-02 19:14:02 +0900

    ✨ Feat: Input 컴포넌트 추가

--- src/components/atoms/Input.tsx
+++ src/components/atoms/Input.tsx
@@ -10,11 +10,12 @@ export default function Input({
   type = "text",
   className = "",
   icon,
-  fontSize = "16", // 기본 16px
+  fontSize = "16", // 기본 text-16px
   ...props
 }: InputProps) {
   return (
     <div className="relative flex items-center">
+      {/* input 왼쪽에 아이콘 위치 */}
       {icon && (
         <span className="absolute left-[10px] pointer-events-none flex items-center">
           {icon}
@@ -22,8 +23,9 @@ export default function Input({
       )}
       <input
         type={type}
+        // 아이콘이 있을 때 왼쪽 패딩 10px 추가
         className={`
-          ${icon ? "pl-10" : ""}
+          ${icon ? "pl-10" : ""} 
           rounded-[5px] border px-[20px] placeholder-[#ababab]
           ${fontSize === "12" ? "text-[12px]" : "text-[16px]"}
           ${className}
