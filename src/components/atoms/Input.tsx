3f82970ab2e55289b62100bd35e8f08c101afad9
kyoeun <lke159357@hufs.ac.kr>
2025-05-05 00:15:13 +0900

    ✨ Feat: Input 수정

--- src/components/atoms/Input.tsx
+++ src/components/atoms/Input.tsx
@@ -2,30 +2,14 @@ import React from "react";
 
 type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
   icon?: React.ReactNode;
-  style?: "short" | "long" | "login" | "comment";
 };
 
 export default function Input({
   type = "text",
   icon,
-  style = "short",
+  className = "",
   ...props
 }: InputProps) {
-  // 댓글 textarea
-  if (style === "comment") {
-    return (
-      <div className="relative flex items-center">
-        <textarea
-          className={`
-            rounded-[5px] border border-[#ababab] px-[20px] pt-[] placeholder-[#ababab] nanum-gothic-regular text-[16px] w-[782px] h-[80px] resize-none
-          `}
-          {...(props as React.TextareaHTMLAttributes<HTMLTextAreaElement>)}
-        />
-      </div>
-    );
-  }
-
-  // input
   return (
     <div className="relative flex items-center">
       {/* 아이콘  */}
@@ -37,12 +21,9 @@ export default function Input({
       <input
         type={type}
         className={`
-          ${icon && "pl-10"}
-
+          ${icon ? "pl-10" : ""}
           rounded-[5px] border px-[20px] placeholder-[#ababab] nanum-gothic-regular text-[16px]
-          ${style === "short" && "w-[255px] h-[38px]"}
-          ${style === "long" && "w-[520px] h-[38px]"}
-          ${style === "login" && "w-[350px] h-[50px]"}
+          ${className}
         `}
         {...props}
       />
