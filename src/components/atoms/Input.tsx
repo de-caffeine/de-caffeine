013793479f459e2d658bdf2931897955b16630e2
kyoeun <lke159357@hufs.ac.kr>
2025-05-04 22:06:07 +0900

    ✨ Feat: Input 수정

--- src/components/atoms/Input.tsx
+++ src/components/atoms/Input.tsx
@@ -1,21 +1,34 @@
 import React from "react";
 
 type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
-  className?: string;
   icon?: React.ReactNode;
-  fontSize?: "16" | "12";
+  style?: "short" | "long" | "login" | "comment";
 };
 
 export default function Input({
   type = "text",
-  className = "",
   icon,
-  fontSize = "16", // 기본 text-16px
+  style = "short",
   ...props
 }: InputProps) {
+  // 댓글 textarea
+  if (style === "comment") {
+    return (
+      <div className="relative flex items-center">
+        <textarea
+          className={`
+            rounded-[5px] border border-[#ababab] px-[20px] pt-[] placeholder-[#ababab] nanum-gothic-regular text-[16px] w-[782px] h-[80px] resize-none
+          `}
+          {...(props as React.TextareaHTMLAttributes<HTMLTextAreaElement>)}
+        />
+      </div>
+    );
+  }
+
+  // input
   return (
     <div className="relative flex items-center">
-      {/* input 왼쪽에 아이콘 위치 */}
+      {/* 아이콘  */}
       {icon && (
         <span className="absolute left-[10px] pointer-events-none flex items-center">
           {icon}
@@ -23,12 +36,13 @@ export default function Input({
       )}
       <input
         type={type}
-        // 아이콘이 있을 때 왼쪽 패딩 10px 추가
         className={`
-          ${icon ? "pl-10" : ""} 
-          rounded-[5px] border px-[20px] placeholder-[#ababab]  nanum-gothic-regular
-          ${fontSize === "12" ? "text-[12px]" : "text-[16px]"}
-          ${className}
+          ${icon && "pl-10"}
+
+          rounded-[5px] border px-[20px] placeholder-[#ababab] nanum-gothic-regular text-[16px]
+          ${style === "short" && "w-[255px] h-[38px]"}
+          ${style === "long" && "w-[520px] h-[38px]"}
+          ${style === "login" && "w-[350px] h-[50px]"}
         `}
         {...props}
       />
