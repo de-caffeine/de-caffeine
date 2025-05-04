4cb64285351e14881108ee055780711ecd2835fb
LEE JUNSU <dlakrp731@gmail.com>
2025-05-04 20:09:23 +0900

    ⚡️ Feat(atoms): Interest 추가

--- src/components/atoms/Interest.tsx
+++ src/components/atoms/Interest.tsx
@@ -3,14 +3,12 @@ import Icon from './Icon';
 export default function Interest({ commentCount = 0, likeCount = 0 }) {
   return (
     <>
-      <div className="flex items-center space-x-4 text-[#000000] text-[12px]">
+      <div className="flex items-center space-x-2 text-[#000000] text-[12px]">
         <div className="flex items-center space-x-1">
-          <Icon name="comment" size={20} />
-          <span>{commentCount}</span>
+          <Icon name="commentIcon" size={20} /> <span>{commentCount}</span>
         </div>
         <div className="flex items-center space-x-1">
-          <Icon name="like" size={20} />
-          <span>{likeCount}</span>
+          <Icon name="unlikeIcon" size={20} /> <span>{likeCount}</span>
         </div>
       </div>
     </>
