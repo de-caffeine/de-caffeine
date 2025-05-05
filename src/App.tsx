33872ed3ae4ffe69ce5d6fdaf21b052bbcd770ed
Hayoung Kang <rkdgkdudsam@naver.com>
2025-05-06 03:50:31 +0900

    Fix: 와일드카드 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,68 @@
-import {Route, Routes} from "react-router-dom" 
-import Header from "./components/organisms/Header";
-import SubNavigation from "./components/atoms/SubNavigation";
-import Main from "./components/templates/Main";
-import NotFound from "./components/templates/NotFound";
-import Community from "./components/templates/Community";
-import Question from "./components/templates/Question";
-import Users from "./components/templates/Users";
-import Search from "./components/templates/Search";
-import Setting from "./components/templates/Setting";
-import MyPage from "./components/templates/MyPage";
-import Post from "./components/templates/Post";
-import Writer from "./components/templates/Writer";
-import UserPage from "./components/templates/UserPage";
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/organisms/Header';
+import SubNavigation from './components/atoms/SubNavigation';
+import Main from './components/templates/Main';
+import NotFound from './components/templates/NotFound';
+import Community from './components/templates/Community';
+import Question from './components/templates/Question';
+import Users from './components/templates/Users';
+import Search from './components/templates/Search';
+import Setting from './components/templates/Setting';
+import MyPage from './components/templates/MyPage';
+import Post from './components/templates/Post';
+import Writer from './components/templates/Writer';
+import UserPage from './components/templates/UserPage';
 
 export default function App() {
   return (
     <>
-      <div className="flex flex-col m-auto items-center">
+      <div className="m-auto flex flex-col items-center">
         <Routes>
-          <Route path="/" element={<Header/>}>
-            <Route path="" element={<Main/>}/>
+          <Route path="/" element={<Header />}>
+            <Route index element={<Main />} />
 
-            <Route path="/community/*" element={<SubNavigation/>}>
-              <Route path="" element={<Community/>}/>
-              <Route path="daily" element={<Community/>}/>
-              <Route path="develop" element={<Community/>}/>
-              <Route path="employ" element={<Community/>}/>
-              <Route path="recruit" element={<Community/>}/>
+            <Route path="/community" element={<SubNavigation />}>
+              <Route index element={<Community />} />
+              <Route path="daily" element={<Community />} />
+              <Route path="develop" element={<Community />} />
+              <Route path="employ" element={<Community />} />
+              <Route path="recruit" element={<Community />} />
             </Route>
 
-            <Route path="/question/*" element={<SubNavigation/>}>
-              <Route path="" element={<Question/>}/>
-              <Route path="solved" element={<Question/>}/>
-              <Route path="unsolved" element={<Question/>}/>
+            <Route path="/question" element={<SubNavigation />}>
+              <Route index element={<Question />} />
+              <Route path="solved" element={<Question />} />
+              <Route path="unsolved" element={<Question />} />
             </Route>
 
-            <Route path="/userpage/my/*" element={<SubNavigation/>}>
-              <Route path="" element={<MyPage/>}/>
-              <Route path="question" element={<MyPage/>}/>
-              <Route path="comment" element={<MyPage/>}/>
-              <Route path="liked" element={<MyPage/>}/>
+            <Route path="/userpage/my" element={<SubNavigation />}>
+              <Route index element={<MyPage />} />
+              <Route path="question" element={<MyPage />} />
+              <Route path="comment" element={<MyPage />} />
+              <Route path="liked" element={<MyPage />} />
             </Route>
 
-            <Route path="/userpage/:userId/*" element={<SubNavigation/>}>
-              <Route path="" element={<UserPage/>}/>
-              <Route path="question" element={<UserPage/>}/>
-              <Route path="comment" element={<UserPage/>}/>
-              <Route path="liked" element={<UserPage/>}/>
+            <Route path="/userpage/:userId" element={<SubNavigation />}>
+              <Route index element={<UserPage />} />
+              <Route path="question" element={<UserPage />} />
+              <Route path="comment" element={<UserPage />} />
+              <Route path="liked" element={<UserPage />} />
             </Route>
 
-            <Route path="/users/*" element={<SubNavigation/>}>
-              <Route path="" element={<Users/>}/>
-              <Route path="online" element={<Users/>}/>
-              <Route path="offline" element={<Users/>}/>
+            <Route path="/users" element={<SubNavigation />}>
+              <Route index element={<Users />} />
+              <Route path="online" element={<Users />} />
+              <Route path="offline" element={<Users />} />
             </Route>
 
-            <Route path="/post/:postId" element={<Post/>}/>
-            <Route path="/writer" element={<Writer/>}/>
+            <Route path="/post/:postId" element={<Post />} />
+            <Route path="/writer" element={<Writer />} />
 
-            <Route path="/search/:keyword" element={<Search/>}/>
-            <Route path="/setting" element={<Setting/>}/>
+            <Route path="/search/:keyword" element={<Search />} />
+            <Route path="/setting" element={<Setting />} />
           </Route>
 
-          <Route path="*" element={<NotFound/>}/>
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
     </>
