c07349c57d55f1a84d96522d5337b181453db4f4
manto123123123 <lhw971103@gmail.com>
2025-05-06 21:21:19 +0900

    ♻️:Refactor name받는부분 props로 받게 수정

--- src/components/atoms/Info.tsx
+++ src/components/atoms/Info.tsx
@@ -1,21 +1,27 @@
 // src/components/molecules/Info.tsx
-import TimeAgo from "../atoms/TimeAgo";
-import UserAvatar from "../atoms/UserAvatar";
+import TimeAgo from '../atoms/TimeAgo';
+import UserAvatar from '../atoms/UserAvatar';
 
 interface InfoProps {
-  imageUrl?: string;
-  size?: number;
-  timestamp: string | number | Date;
+  imageUrl?: string; //UserAvatar 이미지 링크
+  size?: number; //UserAvatar 이미지 사이즈
+  timestamp: string | number | Date; //api를통해 받아온 createdat이 들어옴
+  name: string; // 사용자 이름
 }
 
-export default function Info({ imageUrl, size = 24, timestamp }: InfoProps) {
+export default function Info({
+  imageUrl,
+  size = 24,
+  timestamp,
+  name,
+}: InfoProps) {
   return (
-    <div className="inline-flex items-center space-x-2 flex-shrink-0">
+    <div className="inline-flex flex-shrink-0 items-center space-x-2">
       <UserAvatar imageUrl={imageUrl} size={size} />
       <div className="flex flex-col">
         {/* 이름max-w-[12ch] 넘어가면 뒤에... 표시 */}
-        <span className="text-[16px] nanum-gothic-bold inline-block max-w-[12ch] truncate">
-          저녁뭐먹지ㅋㅋ
+        <span className="nanum-gothic-bold inline-block max-w-[12ch] truncate text-[16px]">
+          {name}
         </span>
         <TimeAgo timestamp={timestamp} />
       </div>
