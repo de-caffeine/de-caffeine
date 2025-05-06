637ae67aed5d8dc66569e305e7dca4c6e3913779
Suji Hwang <soojiya2003@gmail.com>
2025-05-06 23:59:49 +0900

    Feat: UserCard tag  props 변경 & tag 렌더링 개수 2개제한

--- src/components/molecules/UserCard.tsx
+++ src/components/molecules/UserCard.tsx
@@ -46,7 +46,7 @@ export default function UserCard({
                 // Tag value 값이 없을 경우 렌더링 하지 않음
                 t.trim() !== '' && (
                   <li key={t.trim().toUpperCase()}>
-                    <Tag value={t.trim().toUpperCase()} />
+                    <Tag>{t.trim().toUpperCase()}</Tag>
                   </li>
                 ),
             )}
