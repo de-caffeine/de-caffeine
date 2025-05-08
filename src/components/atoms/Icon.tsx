b4ca5363370a9b5283af39d5f5b657d20b4a006d
Hayoung Kang <rkdgkdudsam@naver.com>
2025-05-08 15:43:50 +0900

    Fix: Icon 컴포넌트 수정 및 일부 아이콘 svg로 교체

--- src/components/atoms/Icon.tsx
+++ src/components/atoms/Icon.tsx
@@ -1,3 +1,8 @@
+import HeartIcon from '../../assets/images/heart_icon.svg';
+import HeartFillIcon from '../../assets/images/heart_fill_icon.svg';
+import CommentIcon from '../../assets/images/comment_icon.svg';
+import AddIcon from '../../assets/images/comment_icon.svg';
+
 import alarmIcon from "../../assets/images/alarmIcon.png";
 import chatIcon from "../../assets/images/chatIcon.png";
 import mailIcon from "../../assets/images/mailIcon.png";
@@ -6,21 +11,22 @@ import velogIcon from "../../assets/images/velogIcon.png";
 import homepageIcon from "../../assets/images/homepageIcon.png";
 import completeIcon from "../../assets/images/completeIcon.png";
 import completeNotIcon from "../../assets/images/completeNotIcon.png";
-import commentIcon from "../../assets/images/commentIcon.png";
-import likeIcon from "../../assets/images/likeIcon.png";
-import unlikeIcon from "../../assets/images/unlikeIcon.png";
 
 export default function Icon({
   name,
   onClick,
   size = 24, // 기본 24
+  color = "black",
 }: {
   name: string;
   onClick?: () => void;
   size?: number;
+  color?: "black" | "white" | "red" | "gray";
 }) {
   const src =
-    name === "alarmIcon"
+    name === "plusIcon"
+      ? AddIcon
+      : name === "alarmIcon"
       ? alarmIcon
       : name === "chatIcon"
       ? chatIcon
@@ -37,16 +43,18 @@ export default function Icon({
       : name === "completeNotIcon"
       ? completeNotIcon
       : name === "commentIcon"
-      ? commentIcon
+      ? CommentIcon
       : name === "likeIcon"
-      ? likeIcon
-      : unlikeIcon;
+      ? HeartFillIcon
+      : HeartIcon;
 
   return (
     <>
-      <button type="button" className="cursor-pointer" onClick={onClick}>
-        <img src={src} alt={name} style={{ width: size, height: size }} />
-      </button>
+        <div onClick={onClick}>
+          <img src={src} alt={name} style={{ width: size, height: size, filter: color === "red"
+      ? "invert(19%) sepia(97%) saturate(3200%) hue-rotate(-1deg) brightness(96%)"
+      : color === "white" ? "invert(1)" : color === "gray" ? "invert(1) brightness(0.9) contrast(100%)" : ""}} />
+        </div>
     </>
   );
 }
