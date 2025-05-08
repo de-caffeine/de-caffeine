670707a1f9686d03e176502aca1921c5e433315a
dlakrp123 <dlakrp731@gmail.com>
2025-05-08 15:39:03 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import SubNavigation from './components/atoms/SubNavigation';
-
 export default function App() {
   return <></>;
 }
