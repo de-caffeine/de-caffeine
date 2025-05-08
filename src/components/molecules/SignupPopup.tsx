b857fd28c781fdf77e494099bf1851698b6e79d3
dlakrp123 <dlakrp731@gmail.com>
2025-05-08 17:38:54 +0900

    ⚡️ feat(SignupPopup): Add modal content and integrate into Header component

--- src/components/molecules/SignupPopup.tsx
+++ src/components/molecules/SignupPopup.tsx
@@ -1,7 +1,12 @@
 export default function SignupPopup() {
   return (
     <>
-      <div></div>
+      <div className="fixed inset-0 flex items-center justify-center bg-black/50">
+        <div className="relative rounded-lg bg-white p-5 shadow-inner">
+          <button className="absolute top-1 right-1 h-5 w-5">✕</button>
+          <p>모달 내용</p>
+        </div>
+      </div>
     </>
   );
 }
--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
+import SignupPopup from '../molecules/SignupPopup';
 
 export default function Header() {
   return (
     <>
       <h1>Header Component</h1>
+      <SignupPopup />
       <Outlet />
     </>
   );
