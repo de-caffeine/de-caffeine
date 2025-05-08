505be8aca225b9283b82582123491fbb25649fd6
dlakrp123 <dlakrp731@gmail.com>
2025-05-08 15:49:09 +0900

    Refactor(App): Add new routes for question, user, and users sections with SubNavigation

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,7 +1,10 @@
+import { Outlet } from 'react-router-dom';
+
 export default function Header() {
-    return (
-        <>
-            <h1>Header Component</h1>
-        </>
-    );
+  return (
+    <>
+      <h1>Header Component</h1>
+      <Outlet />
+    </>
+  );
 }
