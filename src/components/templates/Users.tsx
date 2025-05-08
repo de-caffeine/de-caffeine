33db684fbe1785517da4518b78249056a5d96320
Suji Hwang <soojiya2003@gmail.com>
2025-05-09 00:24:30 +0900

    Feat: 온오프라인 정렬 & Link to 추가

--- src/components/templates/Users.tsx
+++ src/components/templates/Users.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { getUsers } from '../../api/users';
 import UserCard from '../molecules/UserCard';
 import { Link } from 'react-router-dom';
@@ -22,6 +22,13 @@ export default function Users() {
     loadData();
   }, []);
 
+  //users 온라인 : 오프라인 정렬
+  const sortedUsers = useMemo(() => {
+    return [...users].sort(
+      (a, b) => (b.isOnline ? 1 : 0) - (a.isOnline ? 1 : 0),
+    );
+  }, [users]);
+
   if (loading) {
     return <p className="py-8 text-center">로딩 중...</p>;
   }
@@ -33,7 +40,7 @@ export default function Users() {
   }
   return (
     <div className="grid grid-cols-1 gap-6 p-6 sm:grid-cols-2 lg:grid-cols-3">
-      {users.map((user) => (
+      {sortedUsers.map((user) => (
         <Link key={user._id} to={`/users/${user._id}`} className="block">
           <UserCard
             UName={user.fullName}
