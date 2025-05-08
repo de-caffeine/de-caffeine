d8783a835415a1f70a99663198a4b70e795de3e0
manto123123123 <lhw971103@gmail.com>
2025-05-08 16:10:28 +0900

    ✨ :feat 댓글박스 만듬

---
+++ src/components/molecules/CommentBox.tsx
@@ -0,0 +1,7 @@
+export default function CommentBox() {
+  return (
+    <>
+      <div className="h-auto w-[979px] rounded-[5px] border border-[#ABABAB] bg-white px-25 py-8"></div>
+    </>
+  );
+}
