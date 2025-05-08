176b00101003bb5cce74c7e787876cc4026c69f3
dlakrp123 <dlakrp731@gmail.com>
2025-05-08 11:36:11 +0900

    Refactor(SubNavigation): Simplify component by removing props and using tabData for navigation

--- src/components/atoms/SubNavigation.tsx
+++ src/components/atoms/SubNavigation.tsx
@@ -1,43 +1,48 @@
-interface SubNavigationProps {
-  selected: string;
-  name?: string; // 선택된 프로필 이름
-  username: string; // 로그인 유저이름
-  activeTab: string;
-  onTabClick: (tab: string) => void;
-}
+import { NavLink, Outlet } from 'react-router-dom';
 
-export default function SubNavigation({
-  selected,
-  name,
-  username,
-  activeTab,
-  onTabClick,
-}: SubNavigationProps) {
-  let tabs: string[] = [];
-  const displayName = name === username ? '나' : name;
+const tabData: Record<string, { name: string; path: string }[]> = {
+  community: [
+    { name: '전체', path: '/community' },
+    { name: '일상공유', path: '/community/daily' },
+    { name: '개발일지', path: '/community/develop' },
+    { name: '취업정보', path: '/community/employ' },
+    { name: '팀원모집', path: '/community/recruit' },
+  ],
+  question: [
+    { name: '전체', path: '/question' },
+    { name: '미해결', path: '/question/solved' },
+    { name: '해결완료', path: '/question/unsolved' },
+  ],
+  users: [
+    { name: '전체', path: '/users' },
+    { name: '온라인', path: '/users/online' },
+    { name: '오프라인', path: '/users/offline' },
+  ],
+};
 
-  if (selected === '커뮤니티') {
-    tabs = ['전체', '일상공유', '개발일지', '취업정보', '팀원모집'];
-  } else if (selected === '코드질문') {
-    tabs = ['전체', '미해결', '해결완료'];
-  } else {
-    // 프로필 이름이 선택된 경우
-    tabs = ['프로필', `${displayName}의 질문`, '작성한 댓글', '좋아요 누른 글'];
-  }
+export default function SubNavigation({ channel }: { channel: string }) {
+  const tabs = tabData[channel];
 
   return (
-    <div className="flex space-x-[25px]">
-      {tabs.map((tab) => (
-        <button
-          key={tab}
-          onClick={() => onTabClick(tab)}
-          className={`text-base relative ${
-            tab === activeTab ? 'text-[#000]' : 'text-[#ababab] '
-          }`}
-        >
-          {tab}
-        </button>
-      ))}
-    </div>
+    <>
+      {tabs && (
+        <nav>
+          <ul className="flex gap-5">
+            {tabs.map(({ name, path }) => (
+              <li key={path} className="cursor-pointer">
+                <NavLink
+                  to={path}
+                  end={path === `/${channel}`} // '전체'일 때만 end 처리
+                  className={({ isActive }) => (isActive ? '' : 'opacity-50')}
+                >
+                  {name}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      )}
+      <Outlet />
+    </>
   );
 }
