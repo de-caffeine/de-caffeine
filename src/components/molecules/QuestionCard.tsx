265005945835bfe07dbd9b80ab04823ce642b731
Hayoung Kang <rkdgkdudsam@naver.com>
2025-05-09 11:15:49 +0900

    Design: QuestionCard 디자인 수정

--- src/components/molecules/QuestionCard.tsx
+++ src/components/molecules/QuestionCard.tsx
@@ -6,21 +6,23 @@ import { Link } from 'react-router-dom';
 
 export default function QuestionCard({ post }: { post: Post }) {
   return (
-    <div className="flex h-[182px] w-[100%] flex-col justify-between rounded-[5px] border border-[#d9d9d9]">
+    <div className="flex h-[190px] w-[100%] flex-col justify-between rounded-[5px] border border-[#d9d9d9]">
       <Link to={`/post/${post._id}`}>
-        <div className="flex flex-col p-[15px]">
-          <div className="inline-flex w-[100%] gap-[5px]">
-            <span className="nanum-gothic-bold max-w-[70%] overflow-hidden text-base break-all text-ellipsis whitespace-nowrap">
-              {JSON.parse(post.title).title}
-            </span>
-            {post.channel.description === '해결완료' && (
-              <Icon name="completeIcon" size={20} />
-            )}
+        <div className="items-between flex flex-col">
+          <div className="flex h-[90px] flex-col p-[15px]">
+            <div className="inline-flex w-[100%] gap-[5px]">
+              <span className="nanum-gothic-bold max-w-[70%] overflow-hidden text-base break-all text-ellipsis whitespace-nowrap">
+                {JSON.parse(post.title).title}
+              </span>
+              {post.channel.description === '해결완료' && (
+                <Icon name="completeIcon" size={20} />
+              )}
+            </div>
+            <div className="nanum-gothic-regular line-clamp-2 text-sm text-[#000000]">
+              {JSON.parse(post.title).body}
+            </div>
           </div>
-          <div className="nanum-gothic-regular line-clamp-2 text-sm text-[#000000]">
-            {JSON.parse(post.title).body}
-          </div>
-          <div className="mb-[10px] flex gap-[5px]">
+          <div className="flex gap-[5px] pl-[15px]">
             {JSON.parse(post.title)
               .tag.split(',')
               .map((t: string) => (
