31f3045def66ff69f8d48a1a10592a5b0e7c2b0b
dlakrp123 <dlakrp731@gmail.com>
2025-05-12 11:41:35 +0900

    .

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -26,7 +26,6 @@ export default function Header() {
           onSwitchToSignup={openSignup}
         />
       )}
-
       {showSignup && (
         <SignupPopup
           onClose={() => setShowSignup(false)}
