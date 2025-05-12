8e274def8b9121bce81e2149e517680da0f79e63
Hayoung Kang <rkdgkdudsam@naver.com>
2025-05-12 16:51:22 +0900

    Feat: Search 페이지 제작

--- src/App.tsx
+++ src/App.tsx
@@ -62,7 +62,12 @@ export default function App() {
             <Route path="/post/:postId" element={<Post />} />
             <Route path="/writer" element={<Writer />} />
 
-            <Route path="/search/:keyword" element={<Search />} />
+            <Route path="/search/:keyword">
+              <Route index element={<Search />} />
+              <Route path="community" element={<Search />} />
+              <Route path="question" element={<Search />} />
+              <Route path="user" element={<Search />} />
+            </Route>
             <Route path="/setting" element={<Setting />} />
           </Route>
 
