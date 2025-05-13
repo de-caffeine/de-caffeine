d6598f20e21a15d294b816978f0967c849f586fe
Hayoung Kang <rkdgkdudsam@naver.com>
2025-05-13 16:14:41 +0900

    Fix: 새로고침 해도 전역변수 초기화 안되게 수정

--- src/loginStore.ts
+++ src/loginStore.ts
@@ -1,15 +1,30 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 type LoginStore = {
-  myInfo: User | object;
-  saveLoginInfo: (info: User) => void;
+  myInfo: User | null;
+  accessToken: string;
+  saveLoginInfo: (info: User, token: string) => void;
   updateLoginInfo: (info: User) => void;
   deleteLoginInfo: () => void;
 };
 
-export const useLoginStore = create<LoginStore>((set) => ({
-  myInfo: {},
-  saveLoginInfo: (info) => set({ myInfo: info }),
-  updateLoginInfo: (info) => set({ myInfo: info }),
-  deleteLoginInfo: () => set({ myInfo: {} }),
-}));
+export const useLoginStore = create<LoginStore>()(
+  persist(
+    (set) => ({
+      myInfo: null,
+      accessToken: '',
+      saveLoginInfo: (info, token) => set({ myInfo: info, accessToken: token }),
+      updateLoginInfo: (info) =>
+        set((state) => ({ myInfo: info, accessToken: state.accessToken })),
+      deleteLoginInfo: () => set({ myInfo: null, accessToken: '' }),
+    }),
+    {
+      name: 'login-store',
+      partialize: (state) => ({
+        myInfo: state.myInfo,
+        accessToken: state.accessToken,
+      }),
+    },
+  ),
+);
