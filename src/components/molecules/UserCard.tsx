f47bcad57e285b4e286db0e5ad9947118a045835
Suji Hwang <soojiya2003@gmail.com>
2025-05-19 02:21:26 +0900

    Fix : UserCard.tsx #46 - tag 빈값 문구 추가 및 tag 유무에 따라 있던 높이 값 차이 제거

--- src/components/molecules/UserCard.tsx
+++ src/components/molecules/UserCard.tsx
@@ -1,58 +1,61 @@
-import Tag from '../atoms/Tag';
 import UserAvatar from '../atoms/UserAvatar';
 import UserName from '../atoms/UserName';
+import Tag from '../atoms/Tag';
 
+// UserCard에 필요한 props 타입 선언
 interface UserCardProps {
   UName: string;
-  followCount?: number;
-  followerCount?: number;
-  tags?: string[];
-  imgUrl?: string;
-  loginStatus?: 'offline' | 'online';
+  followCount: number;
+  followerCount: number;
+  tags: string[];
+  imgUrl: string;
+  loginStatus: 'online' | 'offline';
 }
 
 export default function UserCard({
-  UName = '',
-  followCount = 0,
-  followerCount = 0,
-  tags = [],
-  imgUrl = '',
-  loginStatus = 'offline',
+  UName,
+  followCount,
+  followerCount,
+  tags,
+  imgUrl,
+  loginStatus,
 }: UserCardProps) {
   return (
-    <>
-      <div className="r-[5px] flex h-[140px] w-[310px] gap-4 rounded-[5px] border-[1px] border-[#d9d9d9] p-[20px]">
-        <div className="flex-shrink-0">
-          <UserAvatar
-            size={100}
-            statusSize={20}
-            imageUrl={imgUrl}
-            status={loginStatus}
-          />
-        </div>
-        <div className="flex min-w-0 flex-1 flex-col gap-2">
-          <UserName
-            name={UName}
-            className="nanum-gothic-regular top-0 text-[16px]"
-          />
-          <div className="nanum-gothic-regular mb-2 flex justify-baseline gap-4 text-[12px]">
-            <span>팔로우 {followCount}</span>
-            <span>팔로워 {followerCount}</span>
-          </div>
-          {/* tags 배열로 정보 받아와서 맵핑 */}
-          <ul className="flex flex-wrap gap-0.5">
-            {tags.slice(0, 2).map(
-              (t) =>
-                // Tag value 값이 없을 경우 렌더링 하지 않음
-                t.trim() !== '' && (
-                  <li key={t.trim().toUpperCase()}>
-                    <Tag>{t.trim().toUpperCase()}</Tag>
-                  </li>
-                ),
+    <div className="dark:border-dark-border dark:bg-dark-card dark:text-dark-text flex h-[140px] w-[310px] gap-4 rounded-[5px] border border-[#d9d9d9] p-5">
+      <div className="flex-shrink-0">
+        <UserAvatar
+          size={100}
+          statusSize={20}
+          imageUrl={imgUrl}
+          status={loginStatus}
+        />
+      </div>
+      <div className="flex flex-1 flex-col justify-evenly">
+        <div>
+          <UserName name={UName} className="nanum-gothic-bold text-[16px]" />
+          <div className="fmin-h-[24px] flex items-center">
+            {tags.length > 0 ? (
+              <ul className="flex flex-wrap gap-1 leading-none">
+                {tags.slice(0, 2).map((t) =>
+                  t.trim() !== '' ? (
+                    <li key={t.trim().toUpperCase()}>
+                      <Tag>{t.trim().toUpperCase()}</Tag>
+                    </li>
+                  ) : null,
+                )}
+              </ul>
+            ) : (
+              <span className="h-[22px] text-[12px] text-[#ABABAB]">
+                설정한 기술 스택이 없습니다.
+              </span>
             )}
-          </ul>
+          </div>
+        </div>
+        <div className="nanum-gothic-regular flex items-baseline gap-4 text-[12px]">
+          <span>팔로우 {followCount}</span>
+          <span>팔로워 {followerCount}</span>
         </div>
       </div>
-    </>
+    </div>
   );
 }
